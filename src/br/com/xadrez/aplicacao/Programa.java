986c48eb8c47c5c2385842e20a0b459ba17ea665
Rodney Formaggini <rformaggini@gmail.com>
2019-08-23 22:21:03 -0300

    Classe de Posicao

--- src/br/com/xadrez/aplicacao/Programa.java
+++ src/br/com/xadrez/aplicacao/Programa.java
@@ -1,10 +1,12 @@
 package br.com.xadrez.aplicacao;
 
+import br.com.xadrez.tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Posicao posicao = new Posicao(3 ,5);
+		System.out.println(posicao);
 	}
 
 }
--- src/br/com/xadrez/tabuleiro/Posicao.java
+++ src/br/com/xadrez/tabuleiro/Posicao.java
@@ -1,5 +1,42 @@
 package br.com.xadrez.tabuleiro;
 
 public class Posicao {
-
+	
+	private Integer row;
+	private Integer column;
+	
+	
+	public Posicao() {
+		
+	}
+	
+	public Posicao(Integer row, Integer column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
+	public Integer getRow() {
+		return row;
+	}
+	
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+	
+	public Integer getCollum() {
+		return column;
+	}
+	public void setCollum(Integer column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return "Posicao " + row + ", " + column ;
+	}
+	
+	
+	
+	
 }
