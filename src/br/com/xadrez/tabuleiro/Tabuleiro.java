8a90ccaef96091a93c8db9880756b60e057082fe
Rodney Formaggini <rformaggini@gmail.com>
2023-09-16 16:16:16 -0300

    exception e defense programming

--- src/br/com/xadrez/tabuleiro/Tabuleiro.java
+++ src/br/com/xadrez/tabuleiro/Tabuleiro.java
@@ -10,6 +10,9 @@ public class Tabuleiro {
 	
 	public Tabuleiro(Integer linha, Integer coluna) {
 
+		if(linha < 1 || coluna < 1){
+			throw new TabuleiroException("Error creating board: must be at least 1 row and 1 column.");
+		}
 		this.linha = linha;
 		this.coluna = coluna;
 		pecas = new Peca[linha][coluna];
@@ -20,28 +23,44 @@ public class Tabuleiro {
 		return linha;
 	}
 
-	public void setLinha(Integer linha) {
-		this.linha = linha;
-	}
-
 	public Integer getColuna() {
 		return coluna;
 	}
-
-	public void setColuna(Integer coluna) {
-		this.coluna = coluna;
-	}
 	
 	public Peca	peca(Integer linha, Integer coluna) {
+		if(!posicaoExiste(linha, coluna)){
+			throw new TabuleiroException("Position not on the board.");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	public Peca peca(Posicao posicao) {
+		if(!posicaoExiste(posicao)){
+			throw new TabuleiroException("Position not on the board.");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	public void posicionarPeca(Peca peca, Posicao posicao){
+		if(temUmaPeca(posicao)){
+			throw new TabuleiroException("There is already a piece on position " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+
+	private boolean posicaoExiste(Integer linha, Integer coluna){
+		return linha >= 0 && linha < this.linha && coluna >= 0 && coluna < this.coluna;
+	}
+
+	public boolean posicaoExiste(Posicao posicao){
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+	}
+
+	public boolean temUmaPeca(Posicao posicao){
+		if(!posicaoExiste(posicao)){
+			throw new TabuleiroException("Position not on the board.");
+		}
+		return peca(posicao) != null;
 	}
 }
---
+++ src/br/com/xadrez/tabuleiro/TabuleiroException.java
@@ -0,0 +1,8 @@
+package br.com.xadrez.tabuleiro;
+
+public class TabuleiroException extends RuntimeException{
+
+    public TabuleiroException(String message) {
+        super(message);
+    }
+}
